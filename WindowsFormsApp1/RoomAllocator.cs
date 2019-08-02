fc464ed455392f3599559868d183d05b6aacab31
Alfredo Marquez <alfredo.marquez@gmail.com>
2019-08-02 13:22:32 -0500

    Added unit tests

---
+++ WindowsFormsApp1/RoomAllocator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class RoomAllocator
+    {
+        public class Room
+        {
+            public List<int[]> Intervals { set; get; }
+        }
+
+        public int MinMeetingRooms(int[][] intervals)
+        {
+            List<Room> neededRooms = new List<Room>();
+
+            intervals = intervals.OrderBy(x => x[0]).ToArray();
+
+            foreach (int[] intervalToFit in intervals)
+            {
+                bool allocated = false;
+                foreach (Room room in neededRooms)
+                {
+                    bool fitsInRoom = true;
+                    foreach (int[] roomInterval in room.Intervals)
+                    {
+                        if (roomInterval[0] < intervalToFit[0] && roomInterval[1] > intervalToFit[0])
+                        {
+                            fitsInRoom = false;
+                            break;
+                        }
+
+                        if (roomInterval[0] < intervalToFit[1] && roomInterval[1] > intervalToFit[1])
+                        {
+                            fitsInRoom = false;
+                            break;
+                        }
+
+                        if (roomInterval[0] >= intervalToFit[0] && roomInterval[1] <= intervalToFit[1])
+                        {
+                            fitsInRoom = false;
+                            break;
+                        }
+                    }
+
+                    if (fitsInRoom)
+                    {
+                        allocated = true;
+                        room.Intervals.Add(intervalToFit);
+                        break;
+                    }
+                }
+
+                if (!allocated)
+                {
+                    Room newRoom = new Room();
+                    newRoom.Intervals = new List<int[]>();
+                    newRoom.Intervals.Add(intervalToFit);
+                    neededRooms.Add(newRoom);
+                }
+            }
+
+            return neededRooms.Count;
+        }
+    }
+}
